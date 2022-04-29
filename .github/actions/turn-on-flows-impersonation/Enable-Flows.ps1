5801c83acf2774a52e1557e9b45865a52a089bc2
mraitersw <micaela.raiter@southworks.com>
2022-04-29 10:35:51 -0300

    update workflows

--- .github/actions/turn-on-flows-impersonation/Enable-Flows.ps1
+++ .github/actions/turn-on-flows-impersonation/Enable-Flows.ps1
@@ -13,35 +13,36 @@ function Enable-Flows ($tenantId, $clientId, $clientSecret, $environmentUrl, $so
     $connectionString = "AuthType=ClientSecret;url=$environmentUrl;ClientId=$clientId;ClientSecret=$clientSecret"
     $conn = Get-CrmConnection -ConnectionString $connectionString
     $impersonationConn = Get-CrmConnection -ConnectionString $connectionString
-              
+
     $environmentName = $conn.EnvironmentId
     $solutions = Get-CrmRecords -conn $conn -EntityLogicalName solution -FilterAttribute "uniquename" -FilterOperator "eq" -FilterValue "$solutionName"
     if ($solutions.Count -gt 0) {
         $solutionId = $solutions.CrmRecords[0].solutionid
         $result = Get-CrmRecords -conn $conn -EntityLogicalName solutioncomponent -FilterAttribute "solutionid" -FilterOperator "eq" -FilterValue $solutionId -Fields objectid, componenttype
         $solutionComponents = $result.CrmRecords
-            
+
         $deploymentSettings = Get-Content $deploymentSettingsFile | ConvertFrom-Json
-            
+
         foreach ($connectionRefConfig in $deploymentSettings.ConnectionReferences) {
             if ($connectionRefConfig.LogicalName -ne '' -and $connectionRefConfig.ConnectionId -ne '') {
                 # Get the connection reference
                 $connRefs = Get-CrmRecords -conn $conn -EntityLogicalName connectionreference -FilterAttribute "connectionreferencelogicalname" -FilterOperator "eq" -FilterValue $connectionRefConfig.LogicalName
-                if ($connRefs.Count -gt 0) {      
+                if ($connRefs.Count -gt 0) {
                     # Get connection
                     $connections = Get-AdminPowerAppConnection -EnvironmentName $environmentName -Filter $connectionRefConfig.ConnectionId
-                        
-                    # Get Dataverse systemuserid for the system user that maps to the aad user guid that created the connection 
-                    $systemusers = Get-CrmRecords -conn $conn -EntityLogicalName systemuser -FilterAttribute "azureactivedirectoryobjectid" -FilterOperator "eq" -FilterValue $connections[0].CreatedBy.id
-                    if ($systemusers.Count -gt 0) {
-                        # Impersonate the Dataverse systemuser that created the connection when turning on the flow
-                        $impersonationCallerId = $systemusers.CrmRecords[0].systemuserid
-                        foreach ($solutionComponent in $solutionComponents) {
-                            if ($solutionComponent.componenttype -eq "Workflow") {
-                                $workflow = Get-CrmRecord -conn $conn -EntityLogicalName workflow -Id $solutionComponent.objectid -Fields clientdata, category, statecode
-                                if ($workflow.clientdata.Contains($connectionRefConfig.LogicalName) -and $workflow.statecode -ne "Activated") {
-                                    $impersonationConn.OrganizationWebProxyClient.CallerId = $impersonationCallerId 
-                                    Set-CrmRecordState -conn $impersonationConn -EntityLogicalName workflow -Id $solutionComponent.objectid -StateCode Activated -StatusCode Activated
+                    if ($connections.Count -gt 0) {
+                        # Get Dataverse systemuserid for the system user that maps to the aad user guid that created the connection
+                        $systemusers = Get-CrmRecords -conn $conn -EntityLogicalName systemuser -FilterAttribute "azureactivedirectoryobjectid" -FilterOperator "eq" -FilterValue $connections[0].CreatedBy.id
+                        if ($systemusers.Count -gt 0) {
+                            # Impersonate the Dataverse systemuser that created the connection when turning on the flow
+                            $impersonationCallerId = $systemusers.CrmRecords[0].systemuserid
+                            foreach ($solutionComponent in $solutionComponents) {
+                                if ($solutionComponent.componenttype -eq "Workflow") {
+                                    $workflow = Get-CrmRecord -conn $conn -EntityLogicalName workflow -Id $solutionComponent.objectid -Fields clientdata, category, statecode
+                                    if ($workflow.clientdata.Contains($connectionRefConfig.LogicalName) -and $workflow.statecode -ne "Activated") {
+                                        $impersonationConn.OrganizationWebProxyClient.CallerId = $impersonationCallerId
+                                        Set-CrmRecordState -conn $impersonationConn -EntityLogicalName workflow -Id $solutionComponent.objectid -StateCode Activated -StatusCode Activated
+                                    }
                                 }
                             }
                         }
@@ -50,5 +51,4 @@ function Enable-Flows ($tenantId, $clientId, $clientSecret, $environmentUrl, $so
             }
         }
     }
-    
 }
